2dcc7e57399bffee6e8d4f9fac9fa2a0f335d554
abdullatif <abdullatifjr002@gmail.com>
2024-08-15 11:39:00 +0300

    change base name

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
-  <BrowserRouter basename='task1_front_fullstack'>
+  <BrowserRouter basename='villa'>
     <ScrollToTop />
     <App />
   </BrowserRouter>
